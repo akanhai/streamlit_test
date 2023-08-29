d4944c93fe7141782ae52622ab5ecb012c5c5e4b
akanhai <142408734+akanhai@users.noreply.github.com>
2023-08-29 10:40:14 +0200

    Create mainscript_app.py

---
+++ mainscript_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas
+
+st.title('Dit is mijn titel')
+st.header('1 Dit is mijn eerste kop')
+st.subheader('Dit is een subkop')
+st.caption('Wat is dan een caption?')
+st.text('Zo schrijf je normale tekst')
+st.write('Andere manier van schrijven')
