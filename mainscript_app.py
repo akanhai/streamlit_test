341c6570bd715ed8185ae9c43537755fd6abdddb
akanhai <142408734+akanhai@users.noreply.github.com>
2023-08-29 10:54:33 +0200

    Update mainscript_app.py

--- mainscript_app.py
+++ mainscript_app.py
@@ -3,7 +3,9 @@ import pandas
 
 st.title('Dit is mijn titel')
 st.header('1 Dit is mijn eerste kop')
+st.divider()
 st.subheader('Dit is een subkop')
-st.caption('Wat is dan een caption?')
+st.caption('Een caption is een onderschrift')
 st.text('Zo schrijf je normale tekst')
-st.write('Andere manier van schrijven')
+st.write('Andere manier van schrijven, kan meerdere datatypes in 1 bevatten')
+st.markdown('Zo kun je ook nog teksten schrijven')
